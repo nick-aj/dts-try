d807f2c6690ba1884e28105512c1e4f2df2c70f1
Nicolas Alonso <nick-aj@Nicolass-MacBook-Pro.local>
2023-11-17 10:44:57 +0200

    removing file upload

--- src/index.js
+++ src/index.js
@@ -24,57 +24,30 @@ module.exports = {
    */
   async bootstrap({ strapi }) {
 
-    console.log('BOOTSTRAP!!')
+    // console.log('BOOTSTRAP!!')
 
-    // Download the file and transform it to a buffer
-    // @ts-ignore
-    const response = await fetch('https://via.placeholder.com/600/92c952');
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
+    // // Download the file and transform it to a buffer
+    // // @ts-ignore
+    // const response = await fetch('https://via.placeholder.com/600/92c952');
+    // const arrayBuffer = await response.arrayBuffer();
+    // const buffer = Buffer.from(arrayBuffer);
 
-    // Write the buffer to a file and get the file
-    fs.writeFileSync('./temp_file.png', buffer);
-    //const file = fs.readFileSync('./temp_file.png');
+    // // Write the buffer to a file and get the file
+    // fs.writeFileSync('./temp_file.png', buffer);
 
-    // console.log("Buffer: ", buffer);
-
-    // const config = strapi.config.get('plugin.upload');
-    // const entity = {
-    //   name: "temp_file",
-    //   ext: "png",
+    // const fileMeta = {
+    //   name: "temp_img.png",
     //   mime: "image/png",
-    //   size: bytesToKbytes(Number(file.length)),
-    //   provider: config.provider,
-    //   getStream() {
-    //     return stream.Readable.from(file);
-    //   }
-    // }
+    //   ext: "png",
+    //   type: "image/png",
+    //   buffer: buffer,
+    //   path: "./temp_file.png",
+    //   size: Buffer.byteLength(buffer),
+    // };
 
-    const fileMeta = {
-      name: "temp_img.png",
-      mime: "image/png",
-      ext: "png",
-      type: "image/png",
-      buffer: buffer,
-      path: "./temp_file.png",
-      size: Buffer.byteLength(buffer),
-    };
-
-    const uploadedFile = await strapi.plugin('upload').service('upload').upload({
-      files: fileMeta,
-      data: {}
-    });
-
-
-    // const provider = await strapi.plugin('upload').service('provider');
-    // await getService('provider').upload(file);
-
-    // uploadFile()
-    // const postResponse = await fetch('', {
-    //   method: 'POST',
-    //   body: file
-    // })
-
-    
+    // const uploadedFile = await strapi.plugin('upload').service('upload').upload({
+    //   files: fileMeta,
+    //   data: {}
+    // });
   },
 };
